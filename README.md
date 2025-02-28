7fc43507ba1dcb16341482baeaaa7c9629bb454a
Michael Robinson <kb1dds@gmail.com>
2025-02-28 11:28:56 -0500

    Copyright and attribution

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # stratified_estimator
-Estimating topological and geometric properties of stratified spaces from samples
+Estimating topological and geometric properties of stratified spaces from samples in Euclidean space
+
+# Acknowledgment
+
+This material is based upon work supported by the Defense Advanced Research 
+Projects Agency (DARPA) SafeDocs program under contract HR001119C0072. 
+Any opinions, findings and conclusions or recommendations expressed in 
+this material are those of the authors and do not necessarily reflect the 
+views of DARPA.
--- stratified_estimator/split_geo_estimator.py
+++ stratified_estimator/split_geo_estimator.py
@@ -1,5 +1,25 @@
 #/usr/bin/python3
 # Script to compute local geometric and topological properties of an LLM embedding
+#
+#Copyright (c) 2024-2025 Michael Robinson
+#
+#Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+# this file except in compliance with the License. You may obtain a copy of the 
+#License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#License for the specific language governing permissions and limitations under
+# the License.
+#
+# This material is based upon work supported by the Defense Advanced Research 
+# Projects Agency (DARPA) SafeDocs program under contract HR001119C0072. 
+# Any opinions, findings and conclusions or recommendations expressed in 
+# this material are those of the authors and do not necessarily reflect the 
+# views of DARPA.
 
 import sys
 import argparse
